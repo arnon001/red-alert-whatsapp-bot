985f024ef4fce1bd5cf3e96a64cf1bdecbe8d90d
Arnon Hacohen <arnonhacohen123@gmail.com>
2023-11-22 14:00:13 +0200

    added some help the osm still not working!

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 3. open cmd
 4. run `npm i -g yarn` (If you got yarn, you can skip this stage)
 5. step for ubuntu arm64 users **only!** run: `sudo apt install chromium-browser`
-6. run `yarn install`
+6. run `yarn install` if you have any errors, [click here!](#red-alert-whatsapp-bot)
 7. run `yarn run start`
 
 
@@ -18,5 +18,9 @@ the format should look like this: `972xxxxxxxxx-xxxxxxxxxx@g.us` for group and `
 # Pull Requests
 You are more than welcomed to preform pull requests.
 
+# Installing errors
+If you getting node-gyp errors make sure you installed visual studio 2022 community with desktop development with c++, and also python from the microsoft shop. [Click Here to it!](https://www.microsoft.com/store/productId/9PJPW5LDXLZ5?ocid=pdpshare)
+
+for any other, please open an issue
 # TODO
 - [ ] Map
--- osmtest.js
+++ osmtest.js
@@ -6,15 +6,13 @@ const { typeInHebrew } = require('./functions/typeInHebrew.js');
 const { groupCities } = require('./functions/citiesTime.js');
 const config = require('./config.json');
 const puppeteer = require('puppeteer-extra');
-
 const stealthPlugin = require('puppeteer-extra-plugin-stealth');
+const L = require('leaflet')
 
 puppeteer.use(stealthPlugin());
 
 const groupId = config.sendToUser; // Replace with your group ID
 const interval = 5000;
-
-// Load the session data if it has been previously saved
 
 const client = new Client({
   authStrategy: new LocalAuth()
@@ -26,43 +24,28 @@ client.on('qr', (qrCode) => {
   qrcode.generate(qrCode, { small: true });
 });
 
-
-
 client.on('ready', async () => {
   console.log('Red-Alerts Bot is ready!');
   poll();
 });
 
 async function poll() {
-  // Optional Israeli proxy if running outside Israeli borders
+  setTimeout(poll, interval);
 
-  // Get currently active alert
-  // Example response:
-  // {
-  //    type: 'missiles',
-  //    cities: ['תל אביב - מזרח', 'חיפה - כרמל ועיר תחתית', 'עין גדי'],
-  //    instructions: 'היכנסו למבנה, נעלו את הדלתות וסגרו את החלונות'
-  // }
   pikudHaoref.getActiveAlert(function (err, alert) {
-    // Schedule polling in X millis
-    setTimeout(poll, interval);
-
-    // Log errors
     if (err) {
       return console.log('Retrieving active alert failed: ', err);
     }
 
-    // Cymulate alert
     const test = {
       type: 'missiles',
       cities: ['כיסופים', 'אופקים', 'בארי', 'נחל עוז'],
       instructions: 'היכנסו למבנה, נעלו את הדלתות וסגרו את החלונות',
     };
     sendMessage(test, groupId);
-    console.log("Please stop the program now, or it'll keep sending messages to the user every 5 seconds");
-    // End of Cymulate
 
-    // Line break for readability
+    console.log("Please stop the program now, or it'll keep sending messages to the user every 5 seconds");
+
     console.log();
 
     if (!(JSON.parse(JSON.stringify(alert)).type === `none`) && alertCheck !== alert) {
@@ -73,43 +56,71 @@ async function poll() {
     }
   });
 }
-
 async function generateLeafletMap(markers) {
-const htmlContent = `
+  const groupedMarkers = {};
+
+  markers.forEach(marker => {
+    if (!groupedMarkers[marker.zone]) {
+      groupedMarkers[marker.zone] = [];
+    }
+    groupedMarkers[marker.zone].push(marker);
+  });
+
+  function generateZoneCode(zone, zoneMarkers) {
+    const decodedZone = decodeURIComponent(zone);
+    const encodedMarkers = zoneMarkers.map(marker => ({
+      lat: marker.lat,
+      lng: marker.lng,
+      city: marker.city,
+    }));
+
+    const bounds = zoneMarkers.reduce((acc, marker) => {
+      acc.extend([marker.lat, marker.lng]);
+      return acc;
+    }, new L.LatLngBounds());
+
+    return `
+      var polygonPoints_${decodedZone} = ${JSON.stringify([[bounds.getNorth(), bounds.getWest()], [bounds.getNorth(), bounds.getEast()], [bounds.getSouth(), bounds.getEast()], [bounds.getSouth(), bounds.getWest()]])};
+      var polygon_${decodedZone} = L.polygon(polygonPoints_${decodedZone}, { color: 'red', fillOpacity: 0.3 }).addTo(map);
+
+      ${encodedMarkers.map(marker => {
+        const cityArray = marker.city.split(' ');
+        return `
+          var marker_${decodedZone}_${cityArray.join('_')} = L.marker([${marker.lat}, ${marker.lng}]).addTo(map)
+            .bindPopup('${cityArray.join(' ')}');
+        `;
+      }).join('')}
+    `;
+  }
+
+  const zoneCodes = Object.entries(groupedMarkers).map(([zone, zoneMarkers]) => generateZoneCode(zone, zoneMarkers)).join('\n');
+
+  const htmlContent = `
     <!DOCTYPE html>
     <html>
       <head>
         <title>Leaflet Map</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        <style>
+          #map { height: 100vh; }
+        </style>
       </head>
       <body style="margin: 0; padding: 0;">
-        <div id="map" style="height: 100vh; width: 100%;"></div>
+        <div id="map"></div>
+        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
         <script>
-          // Initialize the map
-          var map = L.map('map').setView([${markers[0].lat}, ${markers[0].lng}], 13); // Adjust the zoom level
+          var map = L.map('map').setView([${markers[0].lat}, ${markers[0].lng}], 13);
 
-          // Add the OpenStreetMap tile layer
           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
           }).addTo(map);
 
-          // Add markers to the map
-          ${markers.map(marker => `
-            L.marker([${marker.lat}, ${marker.lng}]).addTo(map)
-              .bindPopup('${marker.city}');
-            
-            // Add a polygon for each point
-            var polygonPoints${markers.indexOf(marker)} = [[${marker.lat}, ${marker.lng}]];
-            var polygon${markers.indexOf(marker)} = L.polygon(polygonPoints${markers.indexOf(marker)}, { color: 'red', fillOpacity: 0.3 }).addTo(map);
-          `).join('')}
+          ${zoneCodes}
         </script>
       </body>
     </html>
   `;
 
-
-  // Save the HTML content to a temporary file
   const tempDirPath = `${__dirname}/temp`;
   if (!fs.existsSync(tempDirPath)) {
     fs.mkdirSync(tempDirPath);
@@ -118,22 +129,14 @@ const htmlContent = `
   const htmlFilePath = `${tempDirPath}/map.html`;
   fs.writeFileSync(htmlFilePath, htmlContent);
 
-  // Open the HTML file in Puppeteer
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(`file://${htmlFilePath}`, { waitUntil: 'domcontentloaded' });
-
-  // Wait for the map container to appear
   await page.waitForSelector('#map');
-
-  // Wait for a short additional time to ensure the map is fully loaded
   await page.waitForTimeout(3000);
 
-  // Capture a screenshot of the map
   const screenshotPath = `${tempDirPath}/map.png`;
   await page.screenshot({ path: screenshotPath });
-
-  // Close the browser
   await browser.close();
 
   return screenshotPath;
@@ -141,22 +144,14 @@ const htmlContent = `
 
 async function sendLeafletMap(groupId, cities) {
   try {
-    // Check if cities is defined and is an array
     if (!cities || !Array.isArray(cities)) {
       console.error('Cities array is undefined or not an array');
       return;
     }
 
-    // Generate Leaflet map with markers
     const screenshotPath = await generateLeafletMap(cities);
-
-    // Read the image data
     const imageBuffer = fs.readFileSync(screenshotPath);
-
-    // Create a MessageMedia object with the image data
     const media = new MessageMedia('image/png', imageBuffer.toString('base64'));
-
-    // Send the image as a media file with the city names as the caption
     await client.sendMessage(groupId, media, { sendMediaAsSticker: false });
   } catch (error) {
     console.error('Error fetching or sending map image:', error.message);
@@ -165,7 +160,6 @@ async function sendLeafletMap(groupId, cities) {
 }
 
 async function sendMessage(alert, groupId) {
-  // Get current date and time
   const currentDate = new Date();
   const formattedDate = currentDate.toLocaleDateString('en-GB', {
     day: '2-digit',
@@ -179,14 +173,11 @@ async function sendMessage(alert, groupId) {
     second: '2-digit',
   });
 
-  // Group cities based on time and zone using your existing function
   const groupedCities = groupCities(alert.cities);
 
-  // Create the message
   let message = `*🔴 צבע אדום (${formattedDate} | ${formattedTime})*\n`;
   message += `סוג ההתרעה: ${typeInHebrew(alert.type)}\n`;
 
-  // Add cities and towns with latitude and longitude
   message += `ערים וישובים:\n`;
   for (const [zone, cities] of Object.entries(groupedCities)) {
     message += `\n• ${zone}: ${cities.map(city => `${city.city}`).join(', ')} (${cities[0].time})\n`;
@@ -194,10 +185,8 @@ async function sendMessage(alert, groupId) {
 
   message += `\nהוראות: ${alert.instructions}`;
 
-  // Send the message
   client.sendMessage(groupId, message);
 
-  // Send the Leaflet map for the cities mentioned in the alert
   const allCities = Object.values(groupedCities).flat();
   sendLeafletMap(groupId, allCities);
 }
