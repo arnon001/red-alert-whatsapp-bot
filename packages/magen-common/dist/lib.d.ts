a3f10c975b07784e58b668c78e19245b3eac4264
Arnon Hacohen <arnonhacohen123@gmail.com>
2023-12-08 14:20:31 +0200

    Fixed

--- packages/magen-common/dist/lib.d.ts
+++
@@ -1,8 +0,0 @@
-import * as interfaces from './interfaces';
-declare const citiesJson: interfaces.Cities;
-declare const areasJson: interfaces.Areas;
-declare const polygonsJson: interfaces.Polygons;
-declare const threatsJson: interfaces.Threats;
-declare function pointInPolygon(polygon: interfaces.IPolygon, point: [number, number]): boolean;
-export { pointInPolygon, interfaces, threatsJson, citiesJson, areasJson, polygonsJson, };
-//# sourceMappingURL=lib.d.ts.map
